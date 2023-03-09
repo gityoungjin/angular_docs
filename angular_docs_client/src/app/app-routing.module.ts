b0d1f9f669eaad0c945ebbf3a819a19245ed5219
leprintemps <leprintemps7637@gmail.com>
2023-03-09 13:29:37 +0900

    ㅁㅁ

--- angular_docs_client/src/app/app-routing.module.ts
+++ angular_docs_client/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
+import { EditPageComponent } from './feature/edit/page/components/edit-page/edit-page.component';
+import { EditBookComponent } from './feature/edit/book/components/edit-book/edit-book.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'edit/book', component: EditBookComponent },
+  { path: 'edit/page', component: EditPageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- angular_docs_client/src/app/app.module.ts
+++ angular_docs_client/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ChapterComponent } from './shared/components/chapter/chapter.component';
 import { HomeComponent } from './feature/home/home/home.component';
+import { EditBookComponent } from './feature/edit/book/components/edit-book/edit-book.component';
+import { EditPageComponent } from './feature/edit/page/components/edit-page/edit-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ChapterComponent,
-    HomeComponent
+    HomeComponent,
+    EditBookComponent,
+    EditPageComponent
   ],
   imports: [
     BrowserModule,
