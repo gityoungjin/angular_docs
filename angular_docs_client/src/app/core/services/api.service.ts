ab7ddad32c5fd29f25418e4cbdcd8f4a793bbe0c
leprintemps <leprintemps7637@gmail.com>
2023-03-09 13:30:52 +0900

    a

---
+++ angular_docs_client/src/app/core/services/api.service.ts
@@ -0,0 +1,30 @@
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  public BASE_URL = "http://localhost:8080";
+
+  constructor(private http: HttpClient) { }
+
+  get<T>(endPoint: string): Observable<T> {
+    return this.http.get<T>(`${this.BASE_URL}${endPoint}`)
+  }
+
+  post<T>(endPoint: string, body: any): Observable<T> {
+    return this.http.post<T>(`${this.BASE_URL}${endPoint}`, body);
+  }
+
+  put<T>(endPoint: string, body: any): Observable<T> {
+    return this.http.put<T>(`${this.BASE_URL}${endPoint}`, body);
+  }
+
+  delete<T>(endPoint: string): Observable<T> {
+    return this.http.delete<T>(`${this.BASE_URL}${endPoint}`)
+  }
+  
+}
