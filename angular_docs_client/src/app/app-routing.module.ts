93af021aef9cde5f9a13f7c3723b733954aea39e
gityoungjin <totore327@gmail.com>
2023-03-10 10:35:30 +0900

    gh

--- angular_docs_client/src/app/app-routing.module.ts
+++ angular_docs_client/src/app/app-routing.module.ts
@@ -3,8 +3,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BookEditComponent } from './feature/book/book-edit/book-edit.component';
 import { TestComponent } from './feature/test/test.component';
+import { HomeComponent } from './feature/home/home.component';
 
 const routes: Routes = [
+  { path: '', component: HomeComponent },
   { path: 'test', component: TestComponent },
   { path: 'edit/book/:id', component: BookEditComponent },
   { path: 'edit/doc/:id', component: DocEditComponent },
