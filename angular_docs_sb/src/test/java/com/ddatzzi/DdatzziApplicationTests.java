300c896af68a674740f3ce508ca97f94e3ab2293
leprintemps <leprintemps7637@gmail.com>
2023-03-22 17:47:25 +0900

    spring boot 추가함

---
+++ angular_docs_sb/src/test/java/com/ddatzzi/DdatzziApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ddatzzi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DdatzziApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
