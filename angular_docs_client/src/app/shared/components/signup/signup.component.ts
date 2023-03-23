2f221246f99102faf00b66d27358672cedadbad4
gityoungjin <totore327@gmail.com>
2023-03-23 17:44:31 +0900

    0323

---
+++ angular_docs_client/src/app/shared/components/signup/signup.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.scss']
+})
+export class SignupComponent {
+
+}
