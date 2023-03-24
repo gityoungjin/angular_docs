7199fbb1f59f8e617aab516242f4ac8d26657d92
gityoungjin <totore327@gmail.com>
2023-03-24 17:26:22 +0900

    dd

--- angular_docs_client/src/app/shared/components/signup/signup.component.ts
+++ angular_docs_client/src/app/shared/components/signup/signup.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators, } from '@angular/forms';
+import { PasswordValidator } from './password.validator';
 
 @Component({
   selector: 'app-signup',
@@ -8,19 +9,19 @@ import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
 })
 export class SignupComponent {
   userForm!: FormGroup;
-
   ngOnInit() {
     this.userForm = new FormGroup({
       username: new FormControl('', [
         Validators.required,
         Validators.pattern('[a-zA-Z0-9]{4,15}')
       ]),
-      passwordGroup: new FormGroup({
-        password: new FormControl('', Validators.required),
-        confirmPassword: new FormControl('', Validators.required)
-      }),
+      // passwordGroup: new FormGroup({
+        password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+        confirmPassword: new FormControl('',[Validators.required, PasswordValidator.match]),
+      // }, [this.passwordValidator.validate]),
       email: new FormControl('', [
         Validators.required,
+        Validators.email
       ]),
       name: new FormControl('', [
         Validators.required,
